68d0b89c6182bd237beb4f1467da8f971e1648a4
Ryuichi Watanabe <ryucrosskey@gmail.com>
2020-11-07 13:16:59 +0900

    Create iam.tf

---
+++ lambda/iam.tf
@@ -0,0 +1,11 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
